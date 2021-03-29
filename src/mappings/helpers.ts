ccc0ecb046f8f7266a8fb69674adf986984021da
Wen Jing <wenjing@gomefinance.com.cn>
2021-03-29 11:29:35 +0800

    Revert "update"
    This reverts commit 443585d2d0886c30ab57ed07ca2c20de0da44f0d.

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -7,7 +7,7 @@ import { User, Bundle, Token, LiquidityPosition, LiquidityPositionSnapshot, Pair
 import { Factory as FactoryContract } from '../types/templates/Pair/Factory'
 
 export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
-export const FACTORY_ADDRESS = '0xb0b670fc1F7724119963018DB0BfA86aDb22d941'
+export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
 
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
@@ -55,11 +55,11 @@ export function isNullEthValue(value: string): boolean {
 
 export function fetchTokenSymbol(tokenAddress: Address): string {
   // hard coded overrides
-  if (tokenAddress.toHexString() == '0x0298c2b32eae4da002a15f36fdf7615bea3da047') {
-    return 'HUSD'
+  if (tokenAddress.toHexString() == '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a') {
+    return 'DGD'
   }
-  if (tokenAddress.toHexString() == '0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f') {
-    return 'WHT'
+  if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') {
+    return 'AAVE'
   }
 
   let contract = ERC20.bind(tokenAddress)
@@ -85,11 +85,11 @@ export function fetchTokenSymbol(tokenAddress: Address): string {
 
 export function fetchTokenName(tokenAddress: Address): string {
   // hard coded overrides
-  if (tokenAddress.toHexString() == '0x0298c2b32eae4da002a15f36fdf7615bea3da047') {
-    return 'Heco-Peg HUSD Token'
+  if (tokenAddress.toHexString() == '0xe0b7927c4af23765cb51314a0e0521a9645f0e2a') {
+    return 'DGD'
   }
-  if (tokenAddress.toHexString() == '0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f') {
-    return 'Wrapped HT'
+  if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') {
+    return 'Aave Token'
   }
 
   let contract = ERC20.bind(tokenAddress)
@@ -125,10 +125,7 @@ export function fetchTokenTotalSupply(tokenAddress: Address): BigInt {
 
 export function fetchTokenDecimals(tokenAddress: Address): BigInt {
   // hardcode overrides
-  if (tokenAddress.toHexString() == '0x0298c2b32eae4da002a15f36fdf7615bea3da047') {
-    return BigInt.fromI32(8)
-  }
-  if (tokenAddress.toHexString() == '0x5545153ccfca01fbd7dd11c0b23ba694d9509a6f') {
+  if (tokenAddress.toHexString() == '0x7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9') {
     return BigInt.fromI32(18)
   }
 
